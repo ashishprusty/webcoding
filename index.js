5d4307416a95fa853f2f9e5b87cd3bec4330eca0
Ashish <ashisprusty71@gmail.com>
2022-03-22 12:47:44 +0530

    intro to localstorage

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("masai school")
+document.querySelector("button").addEventListener("click", myfunction)
+function myfunction(){
+var name = document.querySelector("#text").value;
+// console.log(name);
+var p = document.createElement("p");
+p.innerText = name;
+document.querySelector("body").append(p)
+}
+
