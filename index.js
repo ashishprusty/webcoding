de9849218221c94d0ee779df8b70a86da53d823a
Ashish <ashisprusty71@gmail.com>
2022-06-07 23:22:54 +0530

    delete

--- index.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("button").addEventListener("click", myfunction)
-function myfunction(){
-var name = document.querySelector("#text").value;
-// console.log(name);
-var p = document.createElement("p");
-p.innerText = name;
-document.querySelector("body").append(p)
-}
-
